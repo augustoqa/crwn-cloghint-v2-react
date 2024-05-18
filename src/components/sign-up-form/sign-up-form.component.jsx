ae9f87bc5c7d36cec12e6cbfbe9f79ba96194ac8
Cesar Quisquinay <augustoacual@gmail.com>
2024-05-18 12:23:54 -0600

    Signing Out

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import FormInput from '../form-input/form-input.component'
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
 import Button from '../button/button.component'
+
+import { UserContext } from '../../contexts/user.context'
 
 import './sign-up-form.styles.scss'
 
@@ -18,6 +20,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const { displayName, email, password, confirmPassword } = formFields
+
+  const { setCurrentUser } = useContext(UserContext)
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields)
@@ -36,6 +40,9 @@ const SignUpForm = () => {
       await createUserDocumentFromAuth(user, {
         displayName,
       })
+
+      setCurrentUser(user)
+
       resetFormFields()
     } catch (error) {
       if (error.code === 'auth/email-already-in-use') {
