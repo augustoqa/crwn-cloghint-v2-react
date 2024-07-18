95b9bf20a3f961afaa04b181f0e7a4ea61599ce4
Cesar Quisquinay <augustoacual@gmail.com>
2024-07-17 21:46:41 -0600

    Create Product Context

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 import './main.scss'
-import {UserProvider} from './contexts/user.context.jsx'
+import { UserProvider } from './contexts/user.context.jsx'
+import { ProductsProvider } from './contexts/products.context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
