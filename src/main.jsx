c1a4e5e56a0d9ffa20b990b09fd5439f1cea7ef7
Augustoa Acual <augustoacual@gmail.com>
2025-10-26 19:01:47 -0600

    React-Redux - Create Category Selector And Use Category Reducer

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 
 import './main.scss'
-import { CategoriesProvider } from './contexts/categories.context.jsx'
 import { CartProvider } from './contexts/cart.context.jsx'
 import { store } from './store/store.js'
 
@@ -14,11 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
