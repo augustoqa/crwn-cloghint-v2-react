7a3d02e1b3a95d6a005be8a7a7afafa1a5e6b14d
Augustoa Acual <augustoacual@gmail.com>
2025-12-25 21:00:22 -0600

    React Redux - Migrate Cart Context to Redux Pt. 2

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,13 @@ import { Provider } from 'react-redux'
 import App from './App.jsx'
 
 import './main.scss'
-import { CartProvider } from './contexts/cart.context.jsx'
 import { store } from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
